96f4d70b6f1824e4ab953a89d3cf3a84e3ebd83d
Vyacheslav Kartashov <ynksdk1654@gmail.com>
2025-02-10 15:07:02 +0300

    .

--- server/api/yandex-music/[...].ts
+++ server/api/yandex-music/[...].ts
@@ -1,21 +1,20 @@
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig();
+    const config = useRuntimeConfig();
 
-  const headers = getRequestHeaders(event);
+    const headers = getRequestHeaders(event);
 
-  let token: string | undefined = headers.authorization?.split(" ")[1];
+    let token: string | undefined = headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    token = config.secretToken || config.public.token;
-  }
+    if (!token) {
+        token = config.secretToken || config.public.token;
+    }
 
-  const path = getRouterParam(event, "_") || "";
-  const url = `https://api.music.yandex.net:443/${path}`;
+    const path = getRouterParam(event, "_") || "";
+    const url = `https://api.music.yandex.net:443/${path}`;
 
-  return proxyRequest(event, url, {
-    headers: {
-      Authorization: `OAuth ${token}`,
-      "Accept-Language": "ru",
-    },
-  });
+    return proxyRequest(event, url, {
+        headers: {
+            Authorization: `OAuth ${token}`, "Accept-Language": "ru",
+        }
+    });
 });
